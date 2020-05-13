1bd9e54ac72f6f75ec33f257498d301c6bbb5ed1
jcLucio <c.lucio3@gmail.com>
2020-05-13 14:03:26 -0500

    Add webpack-dev-server

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,9 +3,5 @@ const common = require('./webpack.common');
 const merge = require('webpack-merge');
 
 module.exports = merge(common, {
-	mode: 'development',
-	output: {
-		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist')
-	}
+	mode: 'development'
 });
