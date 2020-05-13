81229b13dd08fdc76d65a48de3b98191ca58d060
jcLucio <c.lucio3@gmail.com>
2020-05-13 17:20:30 -0500

    Add mini-css-extract-plugin
    - Add plugin to create .css files for production
    - Refactor webpack.prod.js to use MiniCssExtractPlugin.loader
    - Refactor webpack.dev.js to use the default config for handling .scss files from webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -7,17 +7,5 @@ module.exports = {
 		new HtmlWebpackPlugin({
 			template: './src/template.html'
 		})
-	],
-	module: {
-		rules: [
-			{
-				test: /\.s[ac]ss$/i,
-				use: [
-					'style-loader', //3. Inject styles into DOM
-					'css-loader', //2. Turns css into commonjs
-					'sass-loader' //1. Turns sass into css
-				]
-			}
-		]
-	}
+	]
 };
