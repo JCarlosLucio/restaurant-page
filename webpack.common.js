7e22d36f53afff030c9887a2aa3355a670602e51
jcLucio <c.lucio3@gmail.com>
2020-05-13 13:23:44 -0500

    Update webpack to handle .scss files, and add main.scss
    - Added main.scss and imported it on index.js

--- webpack.common.js
+++ webpack.common.js
@@ -5,5 +5,17 @@ module.exports = {
 	output: {
 		filename: 'main.js',
 		path: path.resolve(__dirname, 'dist')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					'style-loader', //3. Inject styles into DOM
+					'css-loader', //2. Turns css into commonjs
+					'sass-loader' //1. Turns sass into css
+				]
+			}
+		]
 	}
 };
