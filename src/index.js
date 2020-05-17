88bc6b3379eba0b691424999fcd1ab375e3aba60
jcLucio <c.lucio3@gmail.com>
2020-05-17 12:46:28 -0500

    Add createMenu to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './main.scss';
 import { createNavbar } from './app/navbar';
 import { createHome } from './app/home';
+import { createMenu } from './app/menu';
 
 createNavbar();
-createHome();
+// createHome();
+createMenu();
