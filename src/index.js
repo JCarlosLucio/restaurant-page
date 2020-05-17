f991716f1bebd0abab625f4ddd0a962f21db5584
jcLucio <c.lucio3@gmail.com>
2020-05-17 14:07:14 -0500

    Add click events to home-link and  menu-link

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import { createHome } from './app/home';
 import { createMenu } from './app/menu';
 
 createNavbar();
-// createHome();
-createMenu();
+const homeLink = document.querySelector('#home-link');
+const menuLink = document.querySelector('#menu-link');
+createHome();
+
+homeLink.addEventListener('click', createHome);
+menuLink.addEventListener('click', createMenu);
