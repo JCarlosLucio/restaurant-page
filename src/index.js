bd7939c7afacb2efb4f58fecf716500646bd91f1
jcLucio <c.lucio3@gmail.com>
2020-05-18 00:22:18 -0500

    Add createContact and add eventlistener to contactLink

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import './main.scss';
 import { createNavbar } from './app/navbar';
 import { createHome } from './app/home';
 import { createMenu } from './app/menu';
+import { createContact } from './app/contact';
 
 createNavbar();
 const homeLink = document.querySelector('#home-link');
 const menuLink = document.querySelector('#menu-link');
+const contactLink = document.querySelector('#contact-link');
 createHome();
 
 homeLink.addEventListener('click', createHome);
 menuLink.addEventListener('click', createMenu);
+contactLink.addEventListener('click', createContact);
