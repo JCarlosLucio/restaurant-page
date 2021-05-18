f203f40fae5961f9a81d06b02de2c0994d6a92f6
jcLucio <c.lucio3@gmail.com>
2021-05-17 19:13:20 -0500

    Update contentHash to contenthash for cacheBusting

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,11 +7,11 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = merge(common, {
   mode: 'production',
   output: {
-    filename: 'main.[contentHash].js',
+    filename: 'main.[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
-    new MiniCssExtractPlugin({ filename: '[name].[contentHash].css' }),
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
     new CleanWebpackPlugin(),
   ],
   module: {
